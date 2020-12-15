f4f7b6415786e6eab786d05c5377f892e6b7f676
jfugalde <vivojf@gmail.com>
2020-12-14 19:44:23 -0600

    Some changes

--- Project1/Proyecto.cpp
+++ Project1/Proyecto.cpp
@@ -93,7 +93,7 @@ int main( )
     
     // Load models
     Model ourModel1((char*)"textures/Van.obj");
-    //Model ourModel2((char*)"Models/Fishes/TropicalFish02.obj");
+    Model ourModel2((char*)"textures/Sky/sky_sphere.obj");
     //Model ourModel3((char*)"Models/Fishes/TropicalFish03.obj");
     //Model ourModel4((char*)"Models/Fishes/TropicalFish04.obj");
     //Model ourModel5((char*)"Models/Fishes/TropicalFish06.obj");
@@ -134,6 +134,7 @@ int main( )
 	
 
             ourModel1.Draw(shader);
+            ourModel2.Draw(shader);
 
         glfwSwapBuffers( window );
     }
