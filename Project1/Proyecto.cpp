9827cfbc5b4f0ecccd3a59aea0b081c3436b28bf
jfugalde <vivojf@gmail.com>
2021-01-06 20:48:00 -0600

    Latest updates

--- Project1/Proyecto.cpp
+++ Project1/Proyecto.cpp
@@ -71,7 +71,7 @@ int main( )
     glfwSetCursorPosCallback( window, MouseCallback );
     
     // GLFW Options
-    //glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
+    glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
     
     // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
